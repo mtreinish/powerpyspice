6977c3bad0fe6f3e0b84a592c47db109eaa50798
Matthew Treinish <mtreinish@kortar.org>
2013-09-01 01:54:46 -0400

    Add power-plot command functionality
    This commit adds functionality to the power-plot command. After
    specifying an input (either spice or hdf) and one or more plots
    to from the input to display it will generate plots using matplotlib.
    This is the cli plotting command. A full fledged waveform editor GUI
    will be added later.

--- powerpyspice/cmd/powerplot.py
+++ powerpyspice/cmd/powerplot.py
@@ -14,12 +14,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import os
 import sys
 
+import h5py
 from oslo.config import cfg
 import pbr.version
 
 from powerpyspice import config
+from powerpyspice.hdf import spice_to_hdf
+from powerpyspice import plot
 
 CONF = cfg.CONF
 
@@ -32,9 +36,21 @@ def main():
         print("Could not read.")
         return(2)
 
-    if CONF.category.name == "version":
+    if not CONF.version:
         print(pbr.version.VersionInfo('powerpyspice'))
         return(0)
 
-    if CONF.category.name == "bash-completion":
-        pass
+    if CONF.spicefile:
+        # TODO(mtreinish) This will create a hdf file and then close it
+        # however we will use it again right after creation so this should
+        # be reworked so that it's not closed until plotting is finished
+        hdf = spice_to_hdf.HdfCreate(CONF.spicefile)
+        h5file = hdf.outfile
+    else:
+        if CONF.hdf_out_dir:
+            h5file = os.path.join(CONF.hdf_out_dir, CONF.hdf_file)
+        else:
+            h5file = CONF.hdf_file
+
+    hdf_file = h5py.File(h5file, "r")
+    plot.display_plot(hdf_file)
--- powerpyspice/plot.py
+++ powerpyspice/plot.py
@@ -14,8 +14,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import matplotlib
-#import numpy
+import matplotlib.lines as lines
+import matplotlib.pyplot
+import matplotlib.text as mtext
+import matplotlib.transforms as mtransforms
 from oslo.config import cfg
 
 CONF = cfg.CONF
@@ -25,8 +27,67 @@ cli_opts = [cfg.StrOpt('renderer',
             short='r',
             default="AGG",
             help="Specify the matplotlib render to use. This affects the"
-                 " output format of the plot. The default is png use agg.")]
+                 " output format of the plot. The default is png use agg."),
+            cfg.MultiStrOpt('plots',
+            short='p',
+            help="Name of plot to be plotted")]
 
 CONF.register_cli_opts(cli_opts)
 
-matplotlib.use(CONF.renderer)
+
+class SpiceLine(lines.Line2D):
+
+    def __init__(self, *args, **kwargs):
+        # we'll update the position when the line data is set
+        self.text = mtext.Text(0, 0, '')
+        super(SpiceLine, self).__init__(*args, **kwargs)
+
+        # we can't access the label attr until *after* the line is
+        # inited
+        self.text.set_text(self.get_label())
+
+    def set_figure(self, figure):
+        self.text.set_figure(figure)
+        lines.Line2D.set_figure(self, figure)
+
+    def set_axes(self, axes):
+        self.text.set_axes(axes)
+        lines.Line2D.set_axes(self, axes)
+
+    def set_transform(self, transform):
+        # 2 pixel offset
+        texttrans = transform + mtransforms.Affine2D().translate(2, 2)
+        self.text.set_transform(texttrans)
+        lines.Line2D.set_transform(self, transform)
+
+    def set_data(self, x, y):
+        if len(x):
+            self.text.set_position((x[-1], y[-1]))
+
+        lines.Line2D.set_data(self, x, y)
+
+    def draw(self, renderer):
+        # draw my label at the end of the line with 2 pixel offset
+        lines.Line2D.draw(self, renderer)
+        self.text.draw(renderer)
+
+
+def display_plot(hdf_file):
+    for plot in CONF.plots:
+        plot_to_graph = hdf_file.get(plot)
+        x = None
+        data_vectors = []
+        for vector in plot_to_graph.values():
+            if 'scale' in vector.name:
+                x = vector[:]
+            else:
+                data_vectors.append(vector)
+        if x is None:
+            print "No scale vector was found in the hdf plot specified"
+            exit(1)
+
+        for vector in data_vectors:
+            y = vector[:]
+            matplotlib.pyplot.plot(x, y, label=vector.attrs['name'])
+    matplotlib.pyplot.legend()
+    matplotlib.pyplot.show()
