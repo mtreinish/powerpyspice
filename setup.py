4281f9c41dfc4d7dc459f51215e0f253228ad6d5
Matthew Treinish <mtreinish@kortar.org>
2016-07-14 01:37:46 -0400

    Make some of the basic build steps work
    This has sat for a very long time without update. Some things went
    stale on just building the sdist. This fixes those problems and
    updates other related things.

--- setup.py
+++ setup.py
@@ -16,6 +16,11 @@
 
 import setuptools
 
+try:
+    import multiprocessing # noqa
+except ImportError:
+    pass
+
 setuptools.setup(
-    setup_requires=['d2to1>=0.2.10,<0.3', 'pbr>=0.5,<0.6'],
-    d2to1=True)
+    setup_requires=['pbr'],
+    pbr=True)
